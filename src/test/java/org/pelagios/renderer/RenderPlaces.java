c5ef7d897bf3bed6144ef8b52d838ebd2155666b
Rainer Simon <rainer.simon@ait.ac.at>
2011-06-27 16:48:29 +0200

    Updated the footprint rendering example

--- src/test/java/org/pelagios/renderer/RenderPlaces.java
+++ src/test/java/org/pelagios/renderer/RenderPlaces.java
@@ -3,6 +3,7 @@ package org.pelagios.renderer;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -10,24 +11,41 @@ import java.util.HashMap;
 
 import javax.imageio.ImageIO;
 
+import org.geotools.geometry.DirectPosition2D;
+import org.opengis.geometry.DirectPosition;
+import org.opengis.geometry.MismatchedDimensionException;
+import org.opengis.referencing.operation.MathTransform;
+import org.opengis.referencing.operation.TransformException;
 import org.pelagios.graph.PelagiosGraph;
 import org.pelagios.graph.Place;
 import org.pelagios.graph.builder.PelagiosGraphBuilder;
+import org.pelagios.rendering.ShapefileRenderer;
 
 /**
  * A utility class that renders the places contained in
  * the Pelagios graph to an image. Points that cover more
  * than one place are drawn in lighter color.
  * 
- * TODO implement this
- * 
- * TODO turn this mess into a decent implementation
+ * TODO turn this into a decent implementation
  * 
  * @author Rainer Simon
  */
 public class RenderPlaces {
 	
+	/**
+	 * Graph DB data directory
+	 */
 	private static final String DATA_DIR = "c:/neo4j-data";
+	
+	/**
+	 * World map shapefile
+	 */
+	private static final String SHAPEFILE = "src\\test\\resources\\110m-coastline\\110m_coastline.shp";
+	
+	/**
+	 * Result file
+	 */
+	private static final String OUTPUT_FILE = "rendered-map.png";
 	
 	private static final HashMap<Point, Integer> points =
 		new HashMap<Point, Integer>();
@@ -38,19 +56,32 @@ public class RenderPlaces {
 	 * 
 	 * @param args no arguments needed
 	 * @throws IOException if anything goes wrong while saving the image
+	 * @throws TransformException 
+	 * @throws MismatchedDimensionException 
 	 */
-	public static void main(String[] args) throws IOException {		
+	public static void main(String[] args)
+		throws IOException, MismatchedDimensionException, TransformException {
+		
+		// Create a 640x400 pixel drawing canvas
+		Rectangle paintArea = new Rectangle(0, 0, 640, 400);
+		BufferedImage bg = new BufferedImage(
+				(int) paintArea.getWidth(),
+				(int) paintArea.getHeight(), 
+				BufferedImage.TYPE_INT_ARGB);
+		Graphics2D g2d = bg.createGraphics();
+		
+		// Render the world map shapefile
+		ShapefileRenderer renderer = new ShapefileRenderer(new File(SHAPEFILE));
+		MathTransform transform = renderer.renderShapefile(g2d, paintArea);
+		
+		// Get a handle on the Pelagios Graph
 		PelagiosGraphBuilder graphBuilder = new PelagiosGraphBuilder(DATA_DIR);
 		PelagiosGraph graph = graphBuilder.build();
 
-		BufferedImage img = new BufferedImage(640, 480, BufferedImage.TYPE_INT_ARGB);
-		Graphics2D g = (Graphics2D) img.getGraphics();
-		g.setPaint(new Color(0, 0, 0));
-		g.fillRect(0, 0, 640, 480);
-		
+		// Add places to the map image
 		int max = 1;
 		for (Place p : graph.listPlaces()) {
-			Point xy = transform(p.getLon(), p.getLat());
+			Point xy = transform(p.getLon(), p.getLat(), transform);
 			if (points.containsKey(xy)) {
 				Integer count = points.get(xy);
 				count++;
@@ -70,16 +101,22 @@ public class RenderPlaces {
 			if (brightness > 255)
 				brightness = 255;
 			int rg = (int) (brightness * 0.5);
-			g.setPaint(new Color(rg, rg, brightness));
-			g.fillOval(p.x - 2, p.y - 2, 3, 3);
+			g2d.setPaint(new Color(brightness, rg, rg));
+			g2d.fillOval(p.x, p.y, 1, 1);
 		}
 		
-		ImageIO.write(img, "PNG", new File("test.png"));
+		graph.shutdown();
+		ImageIO.write(bg, "png", new File(OUTPUT_FILE));  
 	}
 
-	private static Point transform(double lon, double lat) {
-		double x = lon / 180 * 1200 + 160;
-		double y = - lat / 90 * 768 + 540;
-		return new Point((int) x, (int) y);
+	private static Point transform(double lon, double lat, MathTransform transform)
+		throws MismatchedDimensionException, TransformException {
+		
+		DirectPosition src = new DirectPosition2D(lon, lat);
+		DirectPosition dst = new DirectPosition2D(); 
+		dst = transform.transform(src, dst);
+		
+		return new Point((int) dst.getCoordinate()[0], (int) dst.getCoordinate()[1]);
 	}
+	
 }
