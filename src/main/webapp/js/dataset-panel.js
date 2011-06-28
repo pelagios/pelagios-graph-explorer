1f44c1b10ae08a4cfe8d6ed4354d80dc46cea11f
Rainer Simon <rainer.simon@ait.ac.at>
2011-06-28 13:42:56 +0200

    Starting a bare-bones demo webapp

---
+++ src/main/webapp/js/dataset-panel.js
@@ -0,0 +1,27 @@
+window.onload = function () {
+    var p = Raphael("dataset-panel", "100%", "100%");
+
+    var move = function (dx, dy) {
+        this.attr({cx: this.ox + dx, cy: this.oy + dy});
+    };
+    
+    var dragger = function () {
+        this.ox = this.attr("cx");
+        this.oy = this.attr("cy");
+        this.animate({"fill-opacity": .2}, 100);
+    };
+    
+    var up = function () {
+        this.animate({"fill-opacity": 0.8}, 100);
+    };
+    
+	var datasets = new Array();
+    datasets.push(p.ellipse(190, 100, 30, 30));
+    datasets.push(p.ellipse(450, 100, 20, 20));
+	
+    for (var i=0, ii=datasets.length; i<ii; i++) {
+    	datasets[i].attr({fill:"#ff0000", stroke:"#ff0000", "fill-opacity": 0.8, "stroke-width": 2, cursor: "move"});
+    	datasets[i].drag(move, dragger, up);
+    }
+    
+}
