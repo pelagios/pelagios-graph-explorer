8d9cc4dc414b8e14d4f5c6a384cb830d16c4ba26
Rainer Simon <rainer.simon@ait.ac.at>
2011-06-29 15:02:00 +0200

    Removed all springy/Raphael stuff from master branch (is now in the 'springy-raphael' branch)

--- src/main/webapp/js/dataset-panel.js
+++ src/main/webapp/js/dataset-panel.js
@@ -1,135 +1,77 @@
 var graph;
-var datasets;
-var p;
-var connections;
+var nodes;
+var links;
+var force;
+var vis;
 
 var move, dragger, up;
 
 window.onload = function () {
-    p = Raphael("dataset-panel", "100%", "100%");
-
-    // convert to/from screen coordinates
-    var toScreen = function(p) {
-    	var viewport = getViewportSize();
-    	var graph = layout.getBoundingBox();
-    	
-    	var graphSize = graph.topright.subtract(graph.bottomleft);
-    	var sx = p.subtract(graph.bottomleft).divide(graphSize.x).x * viewport.x;
-    	var sy = p.subtract(graph.bottomleft).divide(graphSize.y).y * viewport.y;
-    	return new Vector(sx, sy);
-    };
-
-    fromScreen = function(s) {
-    	var viewport = getViewportSize();
-    	var graph = layout.getBoundingBox();
-    	
-	    var graphSize = graph.topright.subtract(graph.bottomleft);
-	    var px = (s.x / viewport.x) * graphSize.x + graph.bottomleft.x;
-	    var py = (s.y / viewport.y) * graphSize.y + graph.bottomleft.y;
-	    return new Vector(px, py);
-    };
+	nodes = new Array();
+	nodes.push({nodeName:"Ptolemy Machine", group:1, idx:0});
+	// nodes.push({nodeName:"Test2", group:1, idx:1});
+	// nodes.push({nodeName:"Test3", group:2, idx:2});
+	// nodes.push({nodeName:"Test4", group:2, idx:3});
     
-    move = function (dx, dy) {
-        this.attr({cx: this.ox + dx, cy: this.oy + dy});
-        for (var i = connections.length; i--;) {
-            p.connection(connections[i]);
-        }
-        var pt = fromScreen(new Vector(this.ox + dx, this.oy + dy));
-        layout.point(this.graphnode).p = pt;
-        renderer.start(); 
-    };
-    
-    dragger = function () {
-        this.ox = this.attr("cx");
-        this.oy = this.attr("cy");
-        this.animate({"fill-opacity": .2}, 100);
-    };
-    
-    up = function () {
-        this.animate({"fill-opacity": 0.8}, 100);
-    };
-    
-	datasets = new Array();
-    datasets.push(p.ellipse(190, 100, 30, 30));
-    datasets.push(p.ellipse(450, 100, 20, 20));
-    datasets.push(p.ellipse(320, 250, 15, 15));
+    links = new Array();
+    links.push({source:0, target:0, value:1});
+ //  links.push({source:0, target:2, value:1});
+ //  links.push({source:0, target:3, value:5});
+ //  links.push({source:2, target:3, value:20});
 	
-    for (var i=0, ii=datasets.length; i<ii; i++) {
-    	datasets[i].attr({fill:"#ff0000", stroke:"#ff0000", "fill-opacity": 0.8, "stroke-width": 2, cursor: "move"});
-    	datasets[i].drag(move, dragger, up);
-    	datasets[i].dblclick(function (event) {
-    		fetchDatasets(this);
-    	});
-    	datasets[i].name = "Ptolemy Machine";
-    }
-    
-    connections = new Array();
-    connections.push(p.connection(datasets[0], datasets[1], "#000"));
-    connections.push(p.connection(datasets[1], datasets[2], "#000", "#fff|5"));
-    connections.push(p.connection(datasets[0], datasets[2], "#000", "#fff"));
-    
-    // make a new graph
-    graph = new Graph();
-
-    // make some nodes
-    var node1 = graph.newNode();
-    node1.dataset = datasets[0];
-    datasets[0].graphnode = node1;
-    
-    var node2 = graph.newNode();
-    node2.dataset = datasets[1];
-    datasets[1].graphnode = node2;
-    
-    var node3 = graph.newNode();
-    node3.dataset = datasets[2];
-    datasets[2].graphnode = node3;
-    
-    // connect them with an edge
-    graph.newEdge(node1, node2);
-    graph.newEdge(node1, node3);
-    graph.newEdge(node2, node3);
-    
-    var layout = new Layout.ForceDirected(graph, 800.0, 400.0, 0.5);
-    
-    var renderer = new Renderer(10, layout,
-		  function clear() {
-		    // do nothing
-		  },
-		  function drawEdge(edge, p1, p2) {
-		    // do nothing
-		  },
-		  function drawNode(node, pt) {
-			  var xy = toScreen(pt);
-		      for (var i = connections.length; i--;) {
-		    	  p.connection(connections[i]);
-		      }
-			  node.dataset.attr({cx: xy.x, cy: xy.y});
-		  });
-    renderer.start();
+	var w = document.body.clientWidth,
+	    h = document.body.clientHeight,
+	    colors = pv.Colors.category19();
+	
+	vis = new pv.Panel()
+		.width(w)
+		.height(h)
+	    .fillStyle("white")
+	    .event("mousedown", pv.Behavior.pan())
+	    .event("mousewheel", pv.Behavior.zoom());
+	
+	force = vis.add(pv.Layout.Force)
+	    .nodes(nodes)
+	    .links(links)
+	    .chargeConstant(-100)
+	    .springConstant(0.09)
+	    .springLength(70);
+	
+	force.link.add(pv.Line);
+	
+	force.node.add(pv.Dot)
+	    .size(function(n)(n.linkDegree * 20) * Math.pow(this.scale, -1.5))
+	    .fillStyle(function(n) n.fix ? "brown" : colors(n.group))
+	    .strokeStyle(function() this.fillStyle().darker())
+	    .lineWidth(1)
+	    .title(function(d) d.nodeName)
+	    .event("mousedown", pv.Behavior.drag())
+	    .event("drag", force)
+	    .event("dblclick", function(n) fetchDatasets(n))
+    	.add(pv.Label)
+    		.textAlign("center")
+    		.textBaseline("bottom")
+    		.textMargin(function(n) 4 + n.linkDegree / 2)
+    		.text(function(n) n.nodeName);
+	
+	vis.render();
 }
 
 function addDataset(dataset, parent) {
-	// Add to visual representation
-	var blob = p.ellipse(10, 10, 20, 20);
-	blob.attr({fill:"#ff0000", stroke:"#ff0000", "fill-opacity": 0.8, "stroke-width": 2, cursor: "move"});
-	blob.drag(move, dragger, up);
-	blob.dblclick(function (event) {
-		fetchDatasets(this);
-	});
-	blob.name = dataset.name;
-    datasets.push(blob);
-    
-	// Add to graph model
-	var node = graph.newNode();
-	node.dataset = blob;
-	blob.graphnode = node;
+	var newNode = {nodeName:dataset.name, group:1, idx:nodes.length};
+
+	nodes.push(newNode);
+	links.push({source:parent.idx, target:newNode.idx, value:1});
 	
-	if (parent)
-		connections.push(p.connection(blob, parent, "#000"));
+    force.links(links);
+    force.nodes(nodes);
+    
+    force.reset();
+    vis.render();
 }
 
 function fetchDatasets(parent) {
-	$.getJSON("datasets/" + parent.name, function(data) {
+	$.getJSON("datasets/" + parent.nodeName, function(data) {
 		  for (var i=0; i<data.length; i++) {
 			  addDataset(data[i], parent);
 		  }
@@ -137,37 +79,5 @@ function fetchDatasets(parent) {
 	.error(function() { alert("error"); });
 }
 
-function getViewportSize() {
-	 var viewportwidth;
-	 var viewportheight;
-	 
-	 // the more standards compliant browsers (mozilla/netscape/opera/IE7) use window.innerWidth and window.innerHeight
-	 
-	 if (typeof window.innerWidth != 'undefined')
-	 {
-	      viewportwidth = window.innerWidth,
-	      viewportheight = window.innerHeight
-	 }
-	 
-	// IE6 in standards compliant mode (i.e. with a valid doctype as the first line in the document)
-
-	 else if (typeof document.documentElement != 'undefined'
-	     && typeof document.documentElement.clientWidth !=
-	     'undefined' && document.documentElement.clientWidth != 0)
-	 {
-	       viewportwidth = document.documentElement.clientWidth,
-	       viewportheight = document.documentElement.clientHeight
-	 }
-	 
-	 // older versions of IE
-	 
-	 else
-	 {
-	       viewportwidth = document.getElementsByTagName('body')[0].clientWidth,
-	       viewportheight = document.getElementsByTagName('body')[0].clientHeight
-	 }	
-	 
-	 return new Vector(viewportwidth, viewportheight);
-}
 
 
