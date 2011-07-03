ce7fef693a096b15e6764447401c627795385415
Rainer Simon <rainer.simon@ait.ac.at>
2011-07-03 19:39:49 +0200

    Started 'namespaced' Raphael extension for Pelagios-specific drawing elements

--- src/main/webapp/js/dataset-panel.js
+++ src/main/webapp/js/dataset-panel.js
@@ -33,7 +33,7 @@ window.onload = function () {
     move = function (dx, dy) {
         this.attr({cx: this.ox + dx, cy: this.oy + dy});
         for (var i = connections.length; i--;) {
-            p.Connection(connections[i]);
+            p.pelagios.connection(connections[i]);
         }
         var pt = fromScreen(new Vector(this.ox + dx, this.oy + dy));
         layout.point(this.graphnode).p = pt;
@@ -51,18 +51,18 @@ window.onload = function () {
     };
     
 	datasets = new Array();
-    datasets.push(p.Dataset(12));
-    datasets.push(p.Dataset(8));
-    datasets.push(p.Dataset(6));
-    datasets.push(p.Dataset(9));
-    datasets.push(p.Dataset(9));
+    datasets.push(p.pelagios.dataset(12));
+    datasets.push(p.pelagios.dataset(8));
+    datasets.push(p.pelagios.dataset(6));
+    datasets.push(p.pelagios.dataset(9));
+    datasets.push(p.pelagios.dataset(9));
 	
     connections = new Array();
-    connections.push(p.Connection(datasets[0], datasets[1], "#000", 4));
-    connections.push(p.Connection(datasets[1], datasets[2], "#000", 12));
-    connections.push(p.Connection(datasets[0], datasets[2], "#000", 2));
-    connections.push(p.Connection(datasets[0], datasets[3], "#000", 2));
-    connections.push(p.Connection(datasets[0], datasets[4], "#000", 9));
+    connections.push(p.pelagios.connection(datasets[0], datasets[1], "#000", 4));
+    connections.push(p.pelagios.connection(datasets[1], datasets[2], "#000", 12));
+    connections.push(p.pelagios.connection(datasets[0], datasets[2], "#000", 2));
+    connections.push(p.pelagios.connection(datasets[0], datasets[3], "#000", 2));
+    connections.push(p.pelagios.connection(datasets[0], datasets[4], "#000", 9));
     
     for (var i=0, ii=datasets.length; i<ii; i++) {
     	datasets[i].attr({fill:"#9C9EDE", stroke:"#777", "fill-opacity": 1, "stroke-width": 1, cursor: "move"});
@@ -116,7 +116,7 @@ window.onload = function () {
 		  function drawNode(node, pt) {
 			  var xy = toScreen(pt);
 		      for (var i = connections.length; i--;) {
-		    	  p.Connection(connections[i]);
+		    	  p.pelagios.connection(connections[i]);
 		      }
 			  node.dataset.attr({cx: xy.x, cy: xy.y});
 		  });
---
+++ src/main/webapp/js/raphael-pelagios.js
@@ -0,0 +1,55 @@
+/**
+ * PELAGIOS-specific drawing elements, implemented
+ * as a 'namespaced' Raphael extension.
+ */
+Raphael.fn.pelagios = {
+		
+	/**
+	 * A graph node that represents a Pelagios data set
+	 */
+	dataset : function(size, name) {
+		this.name = name;
+		
+		return this
+			.ellipse(0, 0, size, size)
+			.attr({
+				"fill" : "#9C9EDE", 
+				"stroke" : "#777", 
+				"fill-opacity" : 1,
+				"stroke-width" : 1,
+				"cursor": "move"});		
+	},
+
+	/**
+	 * Connection between two graph nodes, based on
+	 * the Raphael 'graffle' demo at
+	 * http://raphaeljs.com/graffle.html  
+	 */
+	connection : function (obj1, obj2, line, width) {
+		if (obj1.line && obj1.from && obj1.to) {
+			line = obj1;
+			obj1 = line.from;
+			obj2 = line.to;
+		}
+		
+		var bb1 = obj1.getBBox(), bb2 = obj2.getBBox();
+
+		var path = "M" + 
+    		(bb1.x + bb1.width / 2) + " " +
+    		(bb1.y + bb1.height / 2) + "L" + 
+    		(bb2.x + bb2.width / 2) + " " + 
+    		(bb2.y + bb2.height / 2);
+    
+		if (line && line.line) {
+			line.bg && line.bg.attr({path: path});
+			line.line.attr({path: path});
+		} else {
+			return {
+				line: this.path(path).attr({"stroke-width":width, opacity:.3, stroke: "#777", fill: "none"}),
+				from: obj1,
+				to: obj2
+			};
+		}
+	}
+
+}
