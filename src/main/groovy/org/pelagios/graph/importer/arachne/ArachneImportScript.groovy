df17e3c7c6f7a893c2e75637e6d97db710e8f815
Rainer Simon <rainer.simon@ait.ac.at>
2011-08-03 15:29:59 +0200

    Arachne import

---
+++ src/main/groovy/org/pelagios/graph/importer/arachne/ArachneImportScript.groovy
@@ -0,0 +1,10 @@
+package org.pelagios.graph.importer.arachne
+
+import org.pelagios.graph.PelagiosGraph;
+
+println("Starting Arachne import")
+def graph = PelagiosGraph.getDefaultDB()
+def arachne = new ArachneImporter(new File("data/datasets/Arachne-PleiadesRDFOrt.n3"))
+arachne.importData(graph)
+graph.shutdown()
+println("Done.")
---
+++ src/main/groovy/org/pelagios/graph/importer/arachne/ArachneImporter.groovy
@@ -0,0 +1,36 @@
+package org.pelagios.graph.importer.arachne
+
+import org.pelagios.graph.PelagiosGraph;
+import org.pelagios.graph.builder.DatasetBuilder;
+import org.pelagios.graph.builder.GeoAnnotationBuilder;
+import org.pelagios.graph.exceptions.DatasetExistsException;
+import org.pelagios.graph.importer.AbstractDatasetImporter;
+
+import com.hp.hpl.jena.rdf.model.Resource;
+
+class ArachneImporter extends AbstractDatasetImporter {
+    
+    public ArachneImporter(File rdf) {
+        super(rdf, new DatasetBuilder("Arachne"), "N3")
+    }
+    
+    @Override
+    public void importData(PelagiosGraph graph) throws DatasetExistsException {
+        graph.addDataset(rootNode)
+        
+        List<GeoAnnotationBuilder> records = new ArrayList<GeoAnnotationBuilder>()
+        for (Resource oac : listOACAnnotations()) {
+            String target = oac.getProperty(OAC_HASTARGET).getObject().toString()
+            
+            GeoAnnotationBuilder annotation = new GeoAnnotationBuilder(
+                new URI(target),
+                new URI(oac.getProperty(OAC_HASBODY).getObject().toString()))
+            
+            annotation.setLabel(target)
+            records.add(annotation);
+        }
+        
+        graph.addGeoAnnotations(records, rootNode)
+    }
+    
+}
