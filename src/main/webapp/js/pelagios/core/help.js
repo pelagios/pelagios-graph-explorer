52738265ce425a65268f6c7f322eb84f93c3fb01
Rainer Simon <rainer.simon@ait.ac.at>
2011-10-21 10:04:34 +0200

    Styling

--- src/main/webapp/js/pelagios/core/help.js
+++ src/main/webapp/js/pelagios/core/help.js
@@ -1,15 +1,16 @@
 Pelagios.Help = { }
 
 Pelagios.Help.TEXT_SEARCHFORM =
-	"The search form...";
+	  "Start typing to search the Pleiades directory ancient places.";
 
 Pelagios.Help.TEXT_SEARCHLIST =
-	  "The search list displays the Places currently in the graph. Use the 'CLEAR'"
-	+ "button to remove all search results. There will be an option to remove single"
-	+ "results later down the road.";
+	  "Displays the list of Places currently being searched. "
+	+ "Click the 'CLEAR' button below to remove all places.";
 
 Pelagios.Help.TEXT_DATAPANEL =
-	"The data panel...";
+	  "Lists the references to the place in the selected data set. "
+	+ "Clicking on a reference will take you to the original data "
+	+ "provider's page.";
 	
 Pelagios.Help.init = function() {
 	var attach = function(id, helpBubble) {
@@ -22,20 +23,20 @@ Pelagios.Help.init = function() {
 		});
 	}
 	
-	attach('search-form', new Pelagios.HelpBubble(Pelagios.Help.TEXT_SEARCHFORM, 275, 185)); 
-    attach('search-list', new Pelagios.HelpBubble(Pelagios.Help.TEXT_SEARCHLIST, 275, 247));
-    attach('data-panel', new Pelagios.HelpBubble(Pelagios.Help.TEXT_DATAPANEL, 275, 440));
+	attach('search-form', new Pelagios.HelpBubble(Pelagios.Help.TEXT_SEARCHFORM, 255, 215)); 
+    attach('search-list', new Pelagios.HelpBubble(Pelagios.Help.TEXT_SEARCHLIST, 255, 280));
+    attach('data-panel', new Pelagios.HelpBubble(Pelagios.Help.TEXT_DATAPANEL, 255, 475));
 }
 
 Pelagios.HelpBubble = function(text, x, y) {
 	this.container = document.createElement("div");
+	this.container.setAttribute("class", "pelagios-help-bubble");
 	this.container.style.position = "absolute";
 	this.container.style.top = y;
 	this.container.style.left = x;
-	this.container.style.backgroundColor = "#ff0000";
 	
 	var box = document.createElement("div");
-	box.setAttribute("class", "pelagios-help-bubble");
+	box.setAttribute("class", "pelagios-help-bubble-inner");
 	box.innerHTML = text;
 	box.style.marginLeft = "12px";
 	box.style.width = "220px";
